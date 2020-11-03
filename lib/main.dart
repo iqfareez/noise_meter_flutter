fec5c8b911de1ef7bc78d04c8457b48366bf5dc1
iqFareez <60868965+fareezMaple@users.noreply.github.com>
2020-11-03 14:38:51 +0800

    Added app icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('Hello world'),);
+  }
+}
 
 
 
