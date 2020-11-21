5cf40af02c75842195b420b4b5e50296682e96b3
iqFareez <60868965+fareezMaple@users.noreply.github.com>
2020-11-21 13:41:43 +0800

    MVP

---
+++ lib/app.dart
@@ -0,0 +1,75 @@
+// https://github.com/syncfusion/flutter-examples/blob/master/lib/samples/chart/dynamic_updates/live_update/real_time_line_chart.dart
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:noise_meter/noise_meter.dart';
+
+class NoiseApp extends StatefulWidget {
+  @override
+  _NoiseAppState createState() => _NoiseAppState();
+}
+
+class _NoiseAppState extends State<NoiseApp> {
+  bool _isRecording = false;
+  StreamSubscription<NoiseReading> _noiseSubscription;
+  NoiseMeter _noiseMeter = NoiseMeter();
+  var maxDB;
+  var meanDB;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void onData(NoiseReading noiseReading) {
+    this.setState(() {
+      if (!this._isRecording) this._isRecording = true;
+    });
+    maxDB = noiseReading.maxDecibel;
+    meanDB = noiseReading.meanDecibel;
+    // print(noiseReading);
+  }
+
+  void start() async {
+    try {
+      _noiseSubscription = _noiseMeter.noiseStream.listen(onData);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void stop() async {
+    try {
+      if (_noiseSubscription != null) {
+        _noiseSubscription.cancel();
+        _noiseSubscription = null;
+      }
+      this.setState(() {
+        this._isRecording = false;
+      });
+    } catch (e) {
+      print('stopRecorder error: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        label: Text(_isRecording ? 'Stop' : 'Start'),
+        onPressed: _isRecording ? stop : start,
+        icon: !_isRecording ? Icon(Icons.circle) : null,
+        backgroundColor: _isRecording ? Colors.red : Colors.green,
+      ),
+      body: Container(
+        child: Center(
+            child: Text(
+          '$maxDB',
+          style: TextStyle(fontSize: 30),
+        )),
+      ),
+    );
+  }
+}
